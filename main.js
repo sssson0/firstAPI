4bffaf3df687214639ed2b4ba2c928aecc591074
sssson0 <s20023094@gmail.com>
2024-02-20 21:38:51 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const API_key ='67d41dbe2f724684a835511fd60169c3';
+let news =[];
+
+const getLatesNews = async()=>{
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_key}`
+    );
+    const response = await fetch(url);
+    const date = await response.json();
+    news = date.articles;
+    console.log('ddd',news);
+}
+
+getLatesNews();
