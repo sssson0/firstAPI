a225bfc49a8a680c8f51f20a71c7f88af9e04212
sssson0 <s20023094@gmail.com>
2024-02-20 22:00:58 +0900

    change url

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_key ='67d41dbe2f724684a835511fd60169c3';
 let news =[];
 
 const getLatesNews = async()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_key}`
+    const url = new URL(`https://neon-licorice-ee838f.netlify.app/top-headlines?country=us&apiKey=${API_key}`
     );
     const response = await fetch(url);
     const date = await response.json();
