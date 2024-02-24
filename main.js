f54aef97070ab3339965a1ccaffb1a5c05184227
sssson0 <s20023094@gmail.com>
2024-02-25 00:44:29 +0900

    토요일

--- main.js
+++ main.js
@@ -5,21 +5,29 @@ const sideMenus = document.querySelectorAll(".side-menu-list");
 let url = new URL(
     `https://neon-licorice-ee838f.netlify.app/top-headlines?country=kr&apiKey=${API_key}`
     );
-
+let totalResults = 0;
+let page = 1;
+const pageSize =10;
+const groupSize = 5;
 
 
 
 const getNews = async() =>{
     try{
+        url.searchParams.set("page",page); //&page = page
+        url.searchParams.set("pageSize",pageSize);
+
         const response = await fetch(url);
-        
         const data = await response.json();
+
         if(response.status === 200) {
             if(data.articles.length === 0){
                 throw new Error("No result for this search!");
             };
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            paginationRender();
         } else{
             throw new Error(data.message);
         }
@@ -113,5 +121,57 @@ const openNav = () => {
 const closeNav = () => {
     document.getElementById("mySidenav").style.width = "0";
 };
+
+const paginationRender = ()=>{
+    const totalPages = Math.ceil(totalResults / pageSize);
+    const pageGroup = Math.ceil(page / groupSize);
+    let lastPage = pageGroup * 5;
+    if(lastPage > totalPages){
+        lastPage = totalPages;
+    }
+
+    let firstPage = lastPage - 4 <=0? 1:lastPage - 4;
+
+    let paginationHTML = ``;
+    if (firstPage >= 6) {
+        paginationHTML = `<li class="page-item" onclick="moveToPage(1)">
+                            <a class="page-link" >&lt;&lt;</a>
+                            </li>
+                            <li class="page-item" onclick="moveToPage(${page - 1})">
+                            <a class="page-link" >&lt;</a>
+                            </li>`;
+        }
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML += `<li class="page-item ${i === page ? "active" : ""}" >
+                            <a class="page-link"  onclick="moveToPage(${i})" >${i}</a>
+                            </li>`;
+        }
+    
+    if (lastPage < totalPages) {
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})">
+                            <a  class="page-link" >&gt;</a>
+                            </li>
+                            <li class="page-item" onclick="moveToPage(${totalPages})">
+                            <a class="page-link" >&gt;&gt;</a>
+                            </li>`;
+    }
+    // for(let i = firstPage; i <= lastPage; i++){
+    //     paginationHTML+=`<li class="page-item ${
+    //         i === page ? "active"  : ""
+    //     }" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    // }
+    // paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">>></a></li>`;
+    document.querySelector(".pagination").innerHTML = paginationHTML;
+
+};
+
+const moveToPage =(pageNum)=>{
+
+    page = pageNum;
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    getNews();
+};
+
 
 getLatesNews();
